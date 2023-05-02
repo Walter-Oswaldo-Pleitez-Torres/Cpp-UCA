0ccff826723a90f646f440499616f3a8df80f0ad
Walter-Oswaldo-Pleitez-Torres <00143023@uca.edu.sv>
2023-05-01 18:29:01 -0600

    V2 Add InicioSesion y Refact_login

---
+++ InicioSesion.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    int intentos=0;
+    string usuario="Walter", user;
+    string password="12345", pwd;
+
+    do{
+        cout<<"Ingrese usuario: ";
+        cin>>user;
+        cout<<"Ingrese clave: ";
+        cin>>pwd;
+
+        bool equal1 = usuario == user;
+        bool equal2 = password == pwd;
+
+        if(equal1 && equal2){
+            cout<<"ACCESO CONCEDIDO"<<endl;
+            break;
+        }
+        else{
+            cout<<"ACCESO DENEGADO"<<endl;
+            cout<<"Le quedan "<<2-intentos<<" intentos."<<endl;
+        }
+
+        cout<<"*****************************"<<endl;
+
+        intentos++;
+    }while(intentos<3);
+
+    return 0;
+}
